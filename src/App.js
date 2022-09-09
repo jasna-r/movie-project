545c2914a0d88d55e0d6494031c03f7b6984e2d1
Jasna R <jassna21@gmail.com>
2022-09-09 03:15:19 +0200

    added component with random list of items

--- src/App.js
+++ src/App.js
@@ -1,91 +1,95 @@
 import React, { useState } from "react";
-// import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Search from "./components/Search/Search";
 import axios from "axios";
 import ResultCard from "./components/ResultCard/ResultCard";
 import Popup from "./components/Popup/Popup";
-// import { Header } from "./components/Header/Header";
-// import { WatchList } from "./components/WatchList";
-// import { Watched } from "./components/Watched";
-// import { Add } from "./components/Add";
+import { ListComponent } from "./components/ListComponent/ListComponent";
 
 function App() {
   const [state, setState] = useState({
-    s: "",
+    searchText: "",
     results: [],
     selected: {},
+    
   });
-  const apiurl = "http://www.omdbapi.com/?i=tt3896198&apikey=79cea0b2";
 
-  const search = (e) => {
-    if(e.key === "Enter") {
-      axios(apiurl + "&s=" + state.s)
-      .then(({ data }) => {
-        let results= data.Search;
-
-        setState(prevState => {
-          return { ...prevState, results: results }
-        })
-      });
+  document.onreadystatechange = () => { 
+    console.log("document state:"+document.readyState);
+    if (document.readyState === 'complete') {
+      console.log("page is loaded");
+      listRandomItem();
     }
   }
+  const apiurl = "http://www.omdbapi.com/?apikey=79cea0b2";
+
+  const search = (e) => {
+    if (e.key === "Enter") {
+      axios(apiurl + "&s=" + state.searchText)
+      .then(({ data }) => {
+        let results = data.Search;
+
+        setState((prevState) => {
+          return { ...prevState, results: results };
+        });
+      });
+    }
+  };
 
   const handleInput = (e) => {
     let s = e.target.value;
 
     setState((prevState) => {
-      return { ...prevState, s: s };
+      return { ...prevState, searchText: s };
     });
-  }
+  };
 
-  const openPopup = id => {
-    axios(apiurl + "&i" + id)
-    .then(({data}) => {
+  const openPopup = (id) => {
+    axios(apiurl + "&i=" + id).then(({ data }) => {
       let result = data;
 
-      setState(prevState => {
-        return { ...prevState, selected: result }
+      setState((prevState) => {
+        return { ...prevState, selected: result };
       });
     });
-  }
+  };
 
   const closePopup = () => {
-    setState(prevState => {
-      return { ...prevState, selected: {} }
+    setState((prevState) => {
+      return { ...prevState, selected: {} };
     });
-  }
+  };
+
+  const listRandomItem = () => {
+    axios(apiurl + "&s=any&y=2015")
+    .then(({ data }) => {
+      let results = data.Search;
+
+      setState((prevState) => {
+        return { ...prevState, results: results };
+
+        
+      });
+      console.log("this is list");
+    });
+  };
 
   return (
     <div className="App">
-      {/* <Router>
-  <Header />
-    <Routes>
-      <Route exact path='/' element={<WatchList/>}>
-        
-      </Route>
-
-      <Route path='/watched' element={<Watched/>}>
-      
-      </Route>
-
-      <Route path='/add' element={<Add/>}>
-        
-      </Route>
-    </Routes>
-
-
- </Router> */}
       <header>
         <h1>Movie Database</h1>
       </header>
       <main>
-        <Search handleInput={handleInput} search= {search} />
+        <Search handleInput={handleInput} search={search} />
+
+        <ListComponent listRandomItem={listRandomItem} />
 
         <ResultCard results={state.results} openPopup={openPopup} />
 
-        {(typeof state.selected.Title != "undefined") ? <Popup selected={state.selected} 
-        closePopup={closePopup} /> :false}
+        {typeof state.selected.Title != "undefined" ? (
+          <Popup selected={state.selected} closePopup={closePopup} />
+        ) : (
+          false
+        )}
       </main>
     </div>
   );
